26bb5f169c4525b9e72988111a6af7e4e41764a1
Anthony <anthony.porter27@gmail.com>
2020-09-23 11:56:05 +0100

    first programme commited

---
+++ VScode/Csharp/first-Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int C;
+            int D;
+            int E;
+            A = 10;
+            B = 20;
+            C = 30;
+            D = A + B;
+            E = C + D;
+            Console.Write("The first numbers added is ");
+            Console.Write(D);
+            Console.Write(", The second set of numbers added is ");
+            Console.Write(E);
+            Console.Read();
+        }
+    }
+}
